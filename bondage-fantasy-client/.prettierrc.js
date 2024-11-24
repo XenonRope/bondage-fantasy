fc22bdc17f825768b25fefbb376a2bd96cbc81f4
XenonRope <xenonrope@example.com>
2024-11-24 14:09:50 +0100

    Add prettier to bondage-fantasy-client

---
+++ bondage-fantasy-client/.prettierrc.js
@@ -0,0 +1,9 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+};
+
+export default config;
