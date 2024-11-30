6d037b9328c9ec36042f6a110f0fcb1b62f672b2
XenonRope <xenonrope@example.com>
2024-11-30 11:53:36 +0100

    Change default language to en

--- bondage-fantasy-client/src/i18n/index.ts
+++ bondage-fantasy-client/src/i18n/index.ts
@@ -5,7 +5,7 @@ import { initReactI18next } from "react-i18next";
 
 i18n.use(initReactI18next).init({
   resources: { en, pl },
-  lng: "pl",
+  lng: "en",
   interpolation: {
     escapeValue: false,
   },
