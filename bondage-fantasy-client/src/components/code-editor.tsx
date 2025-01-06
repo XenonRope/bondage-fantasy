0a9bfcaae0e960ecdc3e49aee8c0b89d0ce55fcd
XenonRope <xenonrope@gmail.com>
2025-01-06 21:42:23 +0100

    Add expression editor

---
+++ bondage-fantasy-client/src/components/code-editor.tsx
@@ -0,0 +1,82 @@
+import { autocompletion } from "@codemirror/autocomplete";
+import { LanguageSupport } from "@codemirror/language";
+import { Input } from "@mantine/core";
+import { useUncontrolled } from "@mantine/hooks";
+import CodeMirror, {
+  EditorState,
+  EditorView,
+  minimalSetup,
+  Transaction,
+} from "@uiw/react-codemirror";
+import { ReactNode } from "react";
+
+const theme = EditorView.theme({
+  "&.cm-focused": {
+    outline: "none",
+  },
+  ".cm-scroller": {
+    fontFamily: "var(--input-font-family)",
+  },
+  ".cm-content": {
+    paddingBottom: "0",
+    paddingTop: "0",
+  },
+  ".cm-line": {
+    padding: "0",
+  },
+});
+
+export function CodeEditor(props: {
+  langauge: LanguageSupport;
+  value?: string;
+  defaultValue?: string;
+  onChange?: (value: string) => void;
+  label?: ReactNode;
+  error?: ReactNode;
+  maxLength?: number;
+  className?: string;
+  classNames?: {
+    input?: string;
+  };
+}) {
+  const [value, handleChange] = useUncontrolled({
+    value: props.value,
+    defaultValue: props.defaultValue,
+    onChange: props.onChange,
+  });
+
+  function isValid(transaction: Transaction): boolean {
+    return (
+      props.maxLength == null || transaction.newDoc.length <= props.maxLength
+    );
+  }
+
+  return (
+    <Input.Wrapper
+      label={props.label}
+      error={props.error}
+      className={props.className}
+    >
+      <Input
+        component="div"
+        multiline={true}
+        error={props.error}
+        classNames={{ input: props.classNames?.input }}
+      >
+        <CodeMirror
+          basicSetup={false}
+          theme={theme}
+          value={value}
+          extensions={[
+            minimalSetup({ drawSelection: false }),
+            EditorState.changeFilter.of(isValid),
+            EditorView.lineWrapping,
+            autocompletion(),
+            props.langauge,
+          ]}
+          onChange={(value) => handleChange(value)}
+        />
+      </Input>
+    </Input.Wrapper>
+  );
+}
--- bondage-fantasy-client/src/components/expression-editor.tsx
+++ bondage-fantasy-client/src/components/expression-editor.tsx
@@ -1,27 +1,22 @@
+import { parser } from "../grammar/expression-parser";
 import { CodeEditor } from "./code-editor";
 import { CompletionContext, CompletionResult } from "@codemirror/autocomplete";
 import { LanguageSupport, LRLanguage } from "@codemirror/language";
-import { parser } from "@grumptech/lezer-mustache";
+import { Operator } from "bondage-fantasy-common";
 import { ReactNode } from "react";
 
 function prepareLanguage() {
   function autocomplete(context: CompletionContext): CompletionResult | null {
-    const word = context.matchBefore(/{{[#/]?[a-zA-Z]*/);
-    if (word == null) {
+    const word = context.matchBefore(/[a-zA-Z]*/);
+    if (
+      word == null ||
+      (!context.tokenBefore(["Operator"]) && !context.explicit)
+    ) {
       return null;
     }
     return {
-      from: ["#", "/"].includes(word.text[2]) ? word.from + 3 : word.from + 2,
-      options: [
-        { label: "name" },
-        { label: "hasVagina" },
-        { label: "hasOnlyVagina" },
-        { label: "hasPenis" },
-        { label: "hasOnlyPenis" },
-        { label: "isFuta" },
-        { label: "sheHer" },
-        { label: "heHim" },
-      ],
+      from: word.from,
+      options: Object.values(Operator).map((operator) => ({ label: operator })),
     };
   }
 
@@ -40,7 +35,7 @@ function prepareLanguage() {
 
 const language = prepareLanguage();
 
-export function TextTemplateEditor(props: {
+export function ExpressionEditor(props: {
   value?: string;
   defaultValue?: string;
   onChange?: (value: string) => void;
