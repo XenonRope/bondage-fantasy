1770296e68b5063ce28be967b75938ef05dabae5
XenonRope <xenonrope@gmail.com>
2025-01-08 22:27:43 +0100

    Improve dependencies

--- bondage-fantasy-client/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-/**
- * @see https://prettier.io/docs/en/configuration.html
- * @type {import("prettier").Config}
- */
-const config = {
-  plugins: ["@trivago/prettier-plugin-sort-imports"],
-};
-
-export default config;
